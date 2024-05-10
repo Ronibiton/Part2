b0a08da799b608f7be38b91c98174f989673f053
Roni Biton <ronibiton05@gmail.com>
2024-05-10 13:50:55 +0300

    added mathematical_operations.py

--- mathematical_operations.py
+++ mathematical_operations.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import pearsonr
 
 
 def get_numbers_from_user() -> []:
@@ -35,33 +36,32 @@ def sort_numbers(numbers: []) -> []:
     return sorted(numbers)
 
 
-def sort_num(numbers: []) -> []:
-    sorted_numbers = []
-    sorted_numbers.append(numbers[0])
-    for i in range(1, len(numbers)):
-        for j in range(len(sorted_numbers)):
-            if numbers[i] <= sorted_numbers[j]:
-                sorted_numbers.insert(j, numbers[i])
-                break
-            elif numbers[i] > sorted_numbers[len(sorted_numbers) - 1]:
-                sorted_numbers.append(numbers[i])
-    return sorted_numbers
-
-###לא בטוחה אם נכון - אם זאת הכוונה
-def build_graph(numbers : [])->None:
+def build_graph(numbers: []) -> None:
     length_numbers = []
-    for i in range(1, len(numbers)+1):
+    for i in range(1, len(numbers) + 1):
         length_numbers.append(i)
     x = np.array(length_numbers)
     y = np.array(numbers)
     plt.scatter(x, y)
     plt.show()
 
-if __name__ == '__main__':
-    # print(get_numbers_from_user())
-    # print(get_avg(get_numbers_from_user()))
-    # print(get_amount_positive_numbers(get_numbers_from_user()))
-    #print(sort_numbers([2, 6, 8, 4, 5, 1, -2, -7]))
-    #print(sort_num([2, 6, 8, 4, 5, 1, -8, 1, -2, -7]))
-    build_graph([2,4,3,6,8])
 
+def pearson(numbers: []):
+    index = []
+    for i in range(1, len(numbers) + 1):
+        index.append(i)
+    pearson_correlation, _ = pearsonr(index, numbers)
+    return pearson_correlation
+
+
+def the_mathematical_operations():
+    numbers = get_numbers_from_user()
+    print(f"the average is: {get_avg(numbers)}")
+    print(f"the input numbers contain {get_amount_positive_numbers(numbers)} positive number/s")
+    print(f"the numbers by sort >>> {sort_numbers(numbers)}")
+    print(f"pearson correlation is: {pearson(numbers)}")
+    build_graph(numbers)
+
+
+if __name__ == '__main__':
+    the_mathematical_operations()
